f8fc0b6d2a895eb61019de80925eef4cf19f2584
andmana <iam.andmana@gmail.com>
2025-02-01 16:49:54 +0700

    Update menu content

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -1,31 +1,42 @@
-import pancake from "../assets/images/pancake.jpg";
-import pizza from "../assets/images/pizza.jpg";
-import steak from "../assets/images/steak.jpg";
-import Spaghetti from "../assets/images/steak.jpg";
+import pancake from "../assets/images/pancake.png";
+import burger from "../assets/images/burger.png";
+import steak from "../assets/images/steak.png";
+import spaghetti from "../assets/images/spaghetti.png";
 
-const menus = ["Steak", "Pizza", "Spaghetti", "Pancake"];
+import luffy2 from "../assets/images/luffy-2.png";
+import "../css/menu.css";
+
+const menus = ["Steak", "Burger", "Spaghetti", "Pancake"];
 const prices = [5, 3, 4, 2];
-const imgs = [steak, pizza, Spaghetti, pancake];
+const imgs = [steak, burger, spaghetti, pancake];
 
 const content = document.getElementById("content");
 
 function loadMenus() {
-    const hero = document.createElement("h2");
-    hero.innerHTML = "Our main dish";
+    const hero = document.createElement("h1");
+    hero.className = "menu-hero";
+    hero.innerHTML = "OUR MAIN DISH";
     content.appendChild(hero);
 
+    const heroImg = document.createElement("img");
+    heroImg.src = luffy2;
+    heroImg.className = "menu-hero-img";
+    content.appendChild(heroImg);
+
     const cards = document.createElement("div");
+    cards.className = "cards";
     content.appendChild(cards);
 
     menus.forEach((menu, index) => {
         const menuCard = document.createElement("div");
+        menuCard.className = "card-item";
         cards.appendChild(menuCard);
 
-        const menuName = document.createElement("h3");
+        const menuName = document.createElement("h1");
         menuName.innerHTML = menu;
         menuCard.appendChild(menuName);
 
-        const menuPrice = document.createElement("span");
+        const menuPrice = document.createElement("h3");
         menuPrice.innerHTML = prices[index] + "$";
         menuCard.appendChild(menuPrice);
 
